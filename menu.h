0b5357ff37e3f3810678069e63e89d4ef236eeb9
José Mário <60543170+MF853@users.noreply.github.com>
2023-08-16 21:44:35 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,7 +1,7 @@
 #ifndef MENU_H
- #define MENU_H
-    void menuManager();
-    void printMenu(int x, int y);
-    void printIntructions(int x, int y);
-    void bbox(WINDOW * window, int  window_rows, int window_cols);
- #endif
+    #define MENU_H
+        void menuManager();
+        void printMenu(int x, int y);
+        void printIntructions(int x, int y);
+        void bbox(WINDOW * window, int  window_rows, int window_cols);
+#endif
